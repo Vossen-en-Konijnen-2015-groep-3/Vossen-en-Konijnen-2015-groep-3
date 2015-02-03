6f5a2d2c7ebb6a52e5245cba5e3bb1f997ff0510
Figueus <j-c-s-s@hotmail.com>
2015-02-03 11:21:18 +0100

    [WIP] Greens
    aka grass iets

---
+++ model/Greens.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.util.List;
+
+public class Greens implements Actor {
+
+	private int size = 5;
+	private int maxSize = 5;
+	
+	@Override
+	public void act(List<Animal> newAnimals) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * Gras groeit terug.
+	 * @param growBack
+	 */
+	private void Growback(int growBack){
+		size = size + growBack;
+		if(size>maxSize){
+			size = maxSize;
+		}
+	}
+	
+	/**
+	 * 
+	 * @param eaten
+	 * @return true als er genoeg eten is andes false
+	 */
+	public boolean eaten(int eaten){
+		if(size - eaten>=0){
+			size = size - eaten;
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+}
